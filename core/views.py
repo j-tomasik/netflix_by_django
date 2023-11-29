cec0d9ece41a267358e35a29056d6e8f3ac4a871
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2023-11-29 15:54:57 -0800

    adding views for user login and signup

--- core/views.py
+++ core/views.py
@@ -4,3 +4,8 @@ from django.shortcuts import render
 def index(request):
   return render(request, 'index.html')
 
+def login(request):
+  return render(request, 'login.html')
+
+def signup(request):
+  return render(request, 'signup.html')

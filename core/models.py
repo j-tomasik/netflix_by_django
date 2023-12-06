39fb7ef29f9edf1dd8c159aa4a953b6db65732ca
Jack Tomasik <jackedwardtomasik@gmail.com>
2023-12-06 15:58:18 -0800

    Creating movie class for db

--- core/models.py
+++ core/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Movie(models.Model):
+    
+    GENRE_CHOICES = [
+        
+    ]
+    
+    title = models.charField(max_length=255)
+    description = models.TextField()
+    release_date = models.DateField()
+    genre = models.CharField(max_length=100, choices=GENRE_CHOICES)

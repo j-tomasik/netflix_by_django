703ca08da514f45c91427f111d7121efdedeb90e
Jack Tomasik <jackedwardtomasik@gmail.com>
2023-12-06 16:18:35 -0800

    adding media folder

--- netflix_site/settings.py
+++ netflix_site/settings.py
@@ -146,3 +146,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+MEDIA_URI = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')

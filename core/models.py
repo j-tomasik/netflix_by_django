0cbcd63a82af52b0687d8da08d03a15dd66d8105
Jack Tomasik <jackedwardtomasik@gmail.com>
2023-12-06 16:16:42 -0800

    finishing up movie model

--- core/models.py
+++ core/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import uuid
 
 # Create your models here.
 class Movie(models.Model):
@@ -13,7 +14,16 @@ class Movie(models.Model):
         ('fantasy', 'Fantasy'),
     ]
     
+    uu_id = models.UUIDField(default=uuid.uuid4)
     title = models.charField(max_length=255)
     description = models.TextField()
     release_date = models.DateField()
     genre = models.CharField(max_length=100, choices=GENRE_CHOICES)
+    length = models.PositiveBigIntegerField()
+    image_card = models.ImageField(upload_to='movie_images/')
+    image_cover = models.ImageField(upload_to='movie_images/')
+    video = models.FilField(upload_to='movie_videos/')
+    movie_views = models.IntegerField(default=0)
+    
+    def __str__(self):
+        return self.title
